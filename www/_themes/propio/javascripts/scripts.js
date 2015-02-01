e07f8a157f2b231449135c603476549ff608181f
Sara Moreno <sara.morenoj@gmail.com>
2015-02-01 20:57:54 +0100

    adding corousel

---
+++ www/_themes/propio/javascripts/scripts.js
@@ -0,0 +1,26 @@
+/*------------------------------------------*\
+    $SCRIPTS
+\*------------------------------------------*/
+
+$(document).ready(function() {
+   
+  $("#owl-demo").owlCarousel({
+   
+  navigation : true, // Show next and prev buttons
+  slideSpeed : 300,
+  paginationSpeed : 400,
+  singleItem:true,
+  items: 3,
+  autoPlay: true
+   
+  // "singleItem:true" is a shortcut for:
+  // items : 1,
+  // itemsDesktop : false,
+  // itemsDesktopSmall : false,
+  // itemsTablet: false,
+  // itemsMobile : false
+   
+  });
+   
+});
+
